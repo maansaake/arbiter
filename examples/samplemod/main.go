5f009fb3d3a7d6ffaaffea351fda2cf600e4e037
Måns Thörnvik <thornvik.mans@gmail.com>
2026-05-15 15:11:24 +0200

    tweaks

--- examples/samplemod/main.go
+++ examples/samplemod/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -13,6 +14,12 @@ func main() {
 	err := arbiter.Run(module.Modules{samplemod.New()})
 	if err == nil {
 		os.Exit(0)
+	}
+
+	// This inspection allows us to provide a more specific error message when the error is related to stopping traffic or modules,
+	if errors.Is(err, arbiter.ErrStopping) {
+		fmt.Fprintf(os.Stderr, "Arbiter stopped with error: %v\n\n", err)
+		os.Exit(1)
 	}
 
 	fmt.Fprintf(os.Stderr, "Error running the Arbiter: %v\n\n", err)
